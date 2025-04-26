0c1f1a977dcff94ed24fb5a6c8500e5c5aaa94ed
Sergey Filippovskikh <116564864+SergikF@users.noreply.github.com>
2025-04-26 23:53:28 +0300

    Выполнение 16 спринта. Корректировка стилей.

--- src/main/java/ru/practicum/shareit/exception/DataConflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class DataConflictException extends RuntimeException {
-    public DataConflictException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/RestrictedAccessException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class RestrictedAccessException extends RuntimeException {
-    public RestrictedAccessException(String message) {
-        super(message);
-    }
-}
