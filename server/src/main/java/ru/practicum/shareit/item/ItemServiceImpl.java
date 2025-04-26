c7b025b7baea1cd06bdb1b6b29490f9cc8a9fab5
Sergey Filippovskikh <116564864+SergikF@users.noreply.github.com>
2025-04-27 00:08:21 +0300

    Выполнение 16 спринта. Корректировка стилей.

--- server/src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -15,7 +15,6 @@ import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserMapper;
 import ru.practicum.shareit.user.UserRepository;
 
-import java.beans.Transient;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
